5c767caddbe6970d7be965d9b8798952e2d8ec3e
Juha Louhiranta <charni@gmail.com>
2012-11-21 16:56:42 +0200

    Remove localStorage item '_hs_hist' instead of hiding elements.

--- js/unpay.js
+++ js/unpay.js
@@ -1,12 +1,5 @@
 "use strict";
 
 (function() {
-    var target = document.querySelector('#article-main-alternative'),
-        paywall = document.querySelectorAll('div.paywall, .paywall-content'),
-        forEach = Array.prototype.forEach;
-    target.style.display = 'block';
-
-    forEach.call(paywall, function(element) {
-        element.style.display = 'none';
-    });
+    localStorage.removeItem('_hs_hist');
 })();
